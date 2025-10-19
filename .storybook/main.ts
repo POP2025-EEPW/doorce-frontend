095eb9845baa7c94c0f48a8b0013f299d1a03fd1
ArionStudio <adrian.rybaczuk.praca@gmail.com>
2025-10-19 22:00:05 +0200

    chore(storybook): update config and remove old mock stories/assets

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -6,6 +8,18 @@ const config: StorybookConfig = {
   framework: {
     name: "@storybook/react-vite",
     options: {},
+  },
+  viteFinal: async (config) => {
+    config.plugins = [
+      ...(config.plugins || []),
+      tsconfigPaths(),
+      tailwindcss(),
+    ];
+    config.define = {
+      ...(config.define || {}),
+      "import.meta.env.VITE_USE_MOCK": JSON.stringify("true"),
+    } as any;
+    return config;
   },
 };
 export default config;
